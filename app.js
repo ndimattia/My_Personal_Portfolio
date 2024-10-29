7a058f300771cc8f29768a9b1c1800c021e02f9c
Noè Di Mattia <noedimattia@protonmail.com>
2024-10-29 19:53:45 +0100

    Updated portrait picture and structure of contents
    Swapped the Positions of the portrait picture and text and also replaced the picture with a professional one.
    
    Removed a sentence in "about me" and also changed the wording of the second sentence to sound better.
    
    restructured the contents so that "Projekte" (projects) comes before "Werdegang" (my cv).
    
    Changed the order of the nav elements to match the structure of the site.
    
    Added class "hidden" to li elments of "Werdegang" to apply scrol animations.
    
    Restructured the description of my previous workplaces.

--- app.js
+++ app.js
@@ -1,23 +1,3 @@
-
-const observer = new IntersectionObserver((entries) => {
-    //checking if entry is visible on the viewport, if true add it to the 'show' class
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        } /* else {
-            entry.target.classList.remove('show');
-        } */
-    });
-});
-
-//detcting all elements of the class 'hidden'
-const hiddenElements = document.querySelectorAll('.hidden');
-
-//observing every entry of the hiddenElements variable
-hiddenElements.forEach((el) => observer.observe(el));
-
-
 //Force to load on top of the page when reloaded. If browser doesn't support scroll restoration, change
 //scroll position to the top before unloading.
 if (history.scrollRestoration) { 
@@ -27,3 +7,28 @@ if (history.scrollRestoration) {
         window.scrollTo(0, 0);
     }
 }
+
+
+//SCROLL ANIMATIONS
+
+//initializing a o
+const observer = new IntersectionObserver((entries) => {
+    //checking if entry is visible on the viewport, if true add it to the 'show' class
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } 
+        /* uncomment if you want the animations to be always active, even after playing them the first time.
+        else {
+            entry.target.classList.remove('show');
+        } 
+        */
+    });
+});
+
+//assigning all elements of the class 'hidden' to the variablehiddenElements.
+const hiddenElements = document.querySelectorAll('.hidden');
+
+//observing every entry of the hiddenElements variable
+hiddenElements.forEach((el) => observer.observe(el));
